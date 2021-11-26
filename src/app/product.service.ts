029ce5beb8ff02254828dfcaf7615c5428c941bf
bldrkamal <bldrkamal@gmail.com>
2021-11-26 13:59:11 +0100

    Completed module

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,8 @@
+import { Album } from './album';
 import { Http, Response } from '@angular/http';
 import { Injectable } from '@angular/core';
 import 'rxjs/add/operator/map';
+
 
 @Injectable()
 export class ProductService {
